4df77c9e52a291831c2b11df124b0c4c32642898
aleymarr <aleyba2002@gmail.com>
2023-06-11 12:15:47 +0100

    feat(actividad1): ejercicio creado y terminado

---
+++ src/com/salesianos/actividad1/Main.java
@@ -0,0 +1,29 @@
+package com.salesianos.actividad1;
+
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String texto = "";
+        final int LONGITUD_MINIMA = 30;
+        FileWriter fileWriter = new FileWriter("src/com/salesianos/ficheros/actividad1.txt");
+
+        while(texto.length() < LONGITUD_MINIMA){
+            System.out.println("Introduce el texto: ");
+            texto = scanner.nextLine();
+            if (texto.length() < LONGITUD_MINIMA){
+                System.out.println("Faltan: " + (LONGITUD_MINIMA - texto.length() + "caracteres"));
+            }
+        }
+
+        String textoFormateado = texto.toLowerCase().replace(" ","_");
+        System.out.println(textoFormateado);
+        fileWriter.write(textoFormateado);
+        fileWriter.close();
+        scanner.close();
+    }
+}
+
